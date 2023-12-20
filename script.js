5d2e0fb1471eab7ebbf01c993a4e9a6fabd911c5
MymekDeveloper <szwed.szymon1@gmail.com>
2023-12-20 22:28:07 +0100

    Added password validation on card display

--- script.js
+++ script.js
@@ -10,3 +10,32 @@ menuOpen.addEventListener("click", () => {
 menuClose.addEventListener("click", () => {
   overlay.classList.remove("overlay--active");
 });
+
+function validatePassword(event) {
+  if (event.key === 'Enter') {
+    event.preventDefault(); // Prevent default form submission
+    var inputElement = document.getElementById('passwordInput');
+    var errorMessageElement = document.getElementById('errorMessage');
+    var successMessageElement = document.getElementById('successMessage');
+    var hashedPassword = hashPassword('HireMe2024'); // Use a secure hashing algorithm
+
+    if (inputElement.value === 'HireMe2024') {
+      errorMessageElement.textContent = '';
+      successMessageElement.textContent = 'Password correct. Page in progress.';
+    } else {
+      errorMessageElement.textContent = 'Incorrect password. Please try again.';
+      successMessageElement.textContent = '';
+    }
+  }
+}
+
+function hashPassword(password) {
+  // Use a secure hashing algorithm (e.g., bcrypt) in a real-world scenario
+  // This example uses a simple hashing method (for demonstration purposes only)
+  var hash = 0;
+  for (var i = 0; i < password.length; i++) {
+    var char = password.charCodeAt(i);
+    hash = (hash << 5) - hash + char;
+  }
+  return hash.toString();
+}
